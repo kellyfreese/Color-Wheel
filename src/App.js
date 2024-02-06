2edff9281b9277f7f5faa3170d2cd6af4174321b
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2024-02-06 10:25:05 -0800

    inital commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const App = () => {
+
+  const [backgroundColor, setBackgroundColor] = useState("#5795b1");
+  const changeBackgroundColor = (e) => {
+    setBackgroundColor(e.target.value);
+  };
+  return (
+    <div className="App">
+      <h2>Select your favorite color!</h2>
+      <div className="color-picker">
+        <p>Click me ▶▶▶</p>
+        <input className="color-input" type="color" value={backgroundColor} onChange={changeBackgroundColor} />
+      </div>
+      <div
+        className="color-square"
+        style={
+          {
+            backgroundColor: backgroundColor/* something goes here */
+          }
+        }
+      ></div>
+    </div>
+  );
+};
+
+export default App;
